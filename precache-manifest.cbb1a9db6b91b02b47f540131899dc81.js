428c3133a640b1f4f7fd7a5f9e6195fbebd54f27
Nikola Milić <milic.n96@gmail.com>
2020-11-23 12:11:09 +0100

    Updates

---
+++ precache-manifest.cbb1a9db6b91b02b47f540131899dc81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ddb4fc0c8864365aed32fcd849d71c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "00a44d220cff9540e507",
+    "url": "./static/css/main.b4200ebb.chunk.css"
+  },
+  {
+    "revision": "ef600937cdd8fad7509d",
+    "url": "./static/js/2.83512f31.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.83512f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a44d220cff9540e507",
+    "url": "./static/js/main.d240a4df.chunk.js"
+  },
+  {
+    "revision": "27407125d5ca42beeae7",
+    "url": "./static/js/runtime-main.b057c790.js"
+  }
+]);
